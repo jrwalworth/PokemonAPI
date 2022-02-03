5c79ff766b3f5da8ac9cd024945ab272cd70f465
Jason Walworth <jrwalworth@gmail.com>
2022-02-02 21:40:58 -0800

    Error handling step 1, API fetch for pokemon abilities and moves.

--- script.js
+++ script.js
@@ -2,35 +2,80 @@
 var pokename = '';
 var pokeData;
 
+
 function getPokename(element) {
     pokename = element.value;
 }
 
-async function calling() {
-    var response = await fetch("https://pokeapi.co/api/v2/pokemon/" + pokename);
-    var pokeData = await response.json();
-    console.log(pokeData);
+async function fetchPokemon() {
+    
+    // Display alert when no input
+    if (pokename ==''){
+        alert('Pokemon name can not be empty')
+    }else {
+        //fetch all Pokemon names
+        // var allResponse = await fetch("https://pokeapi.co/api/v2/pokemon/");
+        // var allPokemon = await allResponse.json();
+        // console.log(allPokemon.results);
+        // var allNames = allPokemon.results;
 
-    document.querySelector(".card-img-top").src = pokeData.sprites.front_shiny;
-    document.querySelector(".card-title").innerText = pokeData.name;
-    document.querySelector(".card-subtitle").innerText = "Weight: " + pokeData.weight + "lbs.";
-    var abilities = [];
-    console.log(pokeData.types.length);
-    for (var i = 0; i < pokeData.types.length; i++) {
-        abilities.push(pokeData.types[i]);
-        console.log(pokeData.types.type);
+        // Display alert when Pokemon not found
+
+
+        //fetch the pokemon data
+        var response = await fetch("https://pokeapi.co/api/v2/pokemon/" + pokename);
+        var pokeData = await response.json();
+        console.log(pokeData.moves);
+        var pokeAbilities = pokeData.abilities;
+        //Display basic info on Pokemon
+        document.querySelector(".card-img-top").src = pokeData.sprites.front_shiny;
+        document.querySelector(".card-title").innerText = pokeData.name;
+        document.querySelector(".card-subtitle").innerText = "Weight: " + pokeData.weight + "lbs.";
+
+        //Gather abilities 
+        var pokemonAbilitiesToDisplay = [];
+        // console.log(pokeData.types.length);
+        for (var i = 0; i < pokeAbilities.length; i++) {
+            pokemonAbilitiesToDisplay.push(" "+pokeData.abilities[i].ability.name);
+            // console.log(pokeAbilities[i].ability.name);
+            // console.log(pokemonAbilitiesToDisplay);
+        }
+        // pokemonAbilitiesToDisplay = pokemonAbilitiesToDisplay.map(x => x+"");
+        //console.log(pokemonAbilitiesToDisplay);
+        document.querySelector(".abilities").innerText = "Abilities: " + pokemonAbilitiesToDisplay;
+
+        //Gather moves
+        var pokeMoves = pokeData.moves;
+        var pokemonMovesToDisplay = [];
+        for (var m = 0; m < pokeMoves.length; m++) {
+            pokemonMovesToDisplay.push(" "+pokeMoves[m].move.name);
+        }
+        document.querySelector(".moves").innerText = "Moves: " + pokemonMovesToDisplay;
+
+
+
+
+
+
+
+        // cardsDiv.innerHTML = makePokeCard(pokeData) + cardsDiv.innerHTML;
+
     }
-    console.log(abilities);
-    document.querySelector(".card-text").innerText = "Abilities: " + pokeData.types.type.name;
 
-    // cardsDiv.innerHTML = makePokeCard(pokeData) + cardsDiv.innerHTML;
+    
 }
 
-function over(element) {
-    document.querySelector(element).src = pokeData.sprites.front_shiny;
+function validateInput() {
+    
 }
 
-function out(element) {
-    document.querySelector(element).src = pokeData.sprites.back_shiny;
 
-}
+
+// function over(element) {
+//     document.querySelector(element).src = pokeData.sprites.front_shiny;
+// }
+
+// function out(element) {
+//     document.querySelector(element).src = pokeData.sprites.back_shiny;
+
+// }
